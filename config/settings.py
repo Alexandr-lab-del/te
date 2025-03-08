0741f31b7ec5b14c8a2aa6d146e02be7471fb1fe
Alexandr-lab-del <gordon675843@mail.ru>
2025-03-09 01:25:53 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -81,16 +81,30 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ex',
+#         'USER': 'postgres',
+#         'PASSWORD': '8695',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ex',
-        'USER': 'postgres',
-        'PASSWORD': '8695',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': os.getenv('POSTGRES_DB', 'ex'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '8695'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
+
+TEST_DATABASE_NAME = os.getenv('TEST_DATABASE_NAME', 'test_db')
+DATABASES['default']['TEST'] = {'NAME': TEST_DATABASE_NAME}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
