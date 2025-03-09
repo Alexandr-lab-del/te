1f2d0f990ab868f135467bd94c4c0c6d847579c9
Alexandr-lab-del <gordon675843@mail.ru>
2025-03-09 12:21:59 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -81,17 +81,6 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ex',
-#         'USER': 'postgres',
-#         'PASSWORD': '8695',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
